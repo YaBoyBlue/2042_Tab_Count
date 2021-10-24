bc9fc01666e08ee8234ccbd8ebafa3edb51f83b6
YaBoyBlue <92649461+YaBoyBlue@users.noreply.github.com>
2021-10-24 13:40:06 -0500

    Initial commit

---
+++ countdown.js
@@ -0,0 +1,39 @@
+const dateA = new Date("Nov 19, 2021 12:00:00")
+const dateB = new Date("Nov 12, 2021 12:00:00")
+const dateC = new Date("Nov 10, 2021 12:00:00")
+
+function getDHMS() {
+
+    let nextRelease = "Pre-Load -"
+    let date = dateC
+
+    if (dateC - new Date() < 0) {
+
+        nextRelease = "Early Access -"
+        date = dateB
+
+        if (dateB - new Date() < 0) {
+
+            nextRelease = "Full Release -"
+            date = dateA
+
+            if (dateA - new Date() < 0) {
+                document.title = "SEE YOU ON THE BATTLEFIELD!"
+                return
+            }
+        }
+    }
+
+    let dateDifference = date - new Date()
+
+    let days = Math.floor(dateDifference / (1000 * 60 * 60 * 24))
+    let hours = Math.floor((dateDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    let minutes = Math.floor((dateDifference % (1000 * 60 * 60)) / (1000 * 60))
+    let seconds = Math.floor((dateDifference % (1000 * 60)) / 1000)
+
+    document.title = `${nextRelease} ${days} : ${hours} : ${minutes} : ${seconds}`
+}
+
+getDHMS()
+
+setInterval(getDHMS,1000)
