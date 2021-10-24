3437f1ee09eab005b4ea178cbcd6437de5f35200
YaBoyBlue <92649461+YaBoyBlue@users.noreply.github.com>
2021-10-24 14:09:54 -0500

    Push

--- countdown.js
+++ countdown.js
@@ -1,18 +1,23 @@
+let smallerTab
+let dateSelect
+
 const dateA = new Date("Nov 19, 2021 12:00:00")
 const dateB = new Date("Nov 12, 2021 12:00:00")
 const dateC = new Date("Nov 10, 2021 12:00:00")
 
 function getDHMS() {
 
+    getSettings()
+
     let nextRelease = "Pre-Load -"
     let date = dateC
 
-    if (dateC - new Date() < 0) {
+    if (dateC - new Date() < 0 || dateSelect == "earlyAccess" || dateSelect == "fullRelease") {
 
         nextRelease = "Early Access -"
         date = dateB
 
-        if (dateB - new Date() < 0) {
+        if (dateB - new Date() < 0  || dateSelect == "fullRelease") {
 
             nextRelease = "Full Release -"
             date = dateA
@@ -24,6 +29,8 @@ function getDHMS() {
         }
     }
 
+    if (smallerTab) {nextRelease = ""}
+
     let dateDifference = date - new Date()
 
     let days = Math.floor(dateDifference / (1000 * 60 * 60 * 24))
@@ -34,6 +41,13 @@ function getDHMS() {
     document.title = `${nextRelease} ${days} : ${hours} : ${minutes} : ${seconds}`
 }
 
+function getSettings() {
+
+    smallerTab = document.querySelector(".smallerTab").checked
+    dateSelect = document.querySelector(".dateSelect").value
+}
+
 getDHMS()
 
 setInterval(getDHMS,1000)
+//setInterval(getSettings, 1000)
